da4660dc3f427588519d5d346b491f7630d4e8a8
Chivu Marius <chivumarius@yahoo.com>
2021-07-30 12:05:12 +0300

    Actualizarea unui Pachet Publicat pe 'npmjs.com''

---
+++ NPM/lion-lib/index.js
@@ -0,0 +1,12 @@
+// EXPORTAREA FUNCTIEI 'ADUNARE()':
+module.exports.adunare = function(a, b) {
+    // SUMA NUMERELOR:
+    return a + b;
+};
+
+
+// EXPORTAREA FUNCTIEI 'INMULTIRE()':
+module.exports.inmultire = function(a, b) {
+    // SUMA NUMERELOR:
+    return a * b;
+};
---
+++ NPM/node-app/index.js
@@ -0,0 +1,8 @@
+//  IMPORTAREA BIBIOTECI CREATE :
+var lion = require('lion-lib-060299');
+
+// APELAM METODA CREATA 'ADAUGARE()':
+var rezultat = lion.adunare(1, 2);
+
+// AFISARE:
+console.log(rezultat);
