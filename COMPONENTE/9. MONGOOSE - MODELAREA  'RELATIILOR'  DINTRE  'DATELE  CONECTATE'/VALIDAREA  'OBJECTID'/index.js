fdeb99a6c894e3cc0e02e8da9815d4005265a348
Chivu Marius <chivumarius@yahoo.com>
2021-09-14 18:05:57 +0300

    Validarea Id-urilor Obiectelor MongoDB

---
+++ COMPONENTE/9. MONGOOSE - MODELAREA  'RELATIILOR'  DINTRE  'DATELE  CONECTATE'/VALIDAREA  'OBJECTID'/index.js
@@ -0,0 +1,64 @@
+// INCARCARI (IMPORTURI):
+const mongoose = require('mongoose');
+const genuri = require('./routes/genuri');
+const clienti = require('./routes/clienti');
+const filme = require('./routes/filme');
+const inchirieri = require('./routes/inchirieri');
+const express = require('express');
+const app = express();
+
+
+
+// ____________________________________________________________________________
+// CONECTAREA  LA  'BAZA DE DATE' - 'MONGODB':
+mongoose.connect('mongodb://localhost/video-rent')
+    // 'PROMISIUNEA' - IN 'CAZ DE SUCCES':
+    .then(() => console.log('Conectat cu succes la Baza de Date MongoDB'))
+    // 'PROMISIUNEA' - IN 'CAZ DE EROARE':
+    .catch(err => console.error('Nu s-a putut conecta la MongoDB.'));
+
+
+
+
+
+
+
+// ____________________________________________________________________________
+// UTILIZARE
+app.use(express.json());
+
+// ____________________________________________________________________________
+// DELEGAREA RUTEI '/API/GENURI' -> CATRE RUTA 'GENYRI'
+app.use('/api/genuri', genuri);
+
+// ____________________________________________________________________________
+// DELEGAREA RUTEI '/API/CLIENTI' -> CATRE RUTA 'CLIENTI'
+app.use('/api/clienti', clienti);
+
+// ____________________________________________________________________________
+// DELEGAREA RUTEI '/API/FILME' -> CATRE RUTA 'FILME'
+app.use('/api/filme', filme);
+
+// ____________________________________________________________________________
+// DELEGAREA RUTEI '/API/INCHIRIERI' -> CATRE RUTA 'INCHIRIERI'
+app.use('/api/inchirieri', inchirieri);
+
+
+
+
+
+
+
+// ____________________________________________________________________________
+// VARIABILA 'ENVIRONMENT' - 'PORT'  SAu  PORTUL '3000':
+// (IN OBIECTUL GLOBAL 'PROCESS', IN PROP. 'ENV')
+const port = process.env.PORT || 3000;
+
+
+// ____________________________________________________________________________
+// MET. 'LISTEN(NR_PORT, CALLBACK FUNC*())' 
+// ASCULTAREA 'VARIABILEI ENVIRONMENT':
+app.listen(port, () => {
+    // 'TEMPLATE  STRING' (PRIN UTIL. 'BACKTICK')
+    console.log(`Port de Ascultare ${port}...`);
+});
