782d111ec3fa8be3d4f51c0ac5413addf4212961
Chivu Marius <chivumarius@yahoo.com>
2021-09-18 19:36:20 +0300

    Setarea 'Header'-elor de 'Raspuns'

--- COMPONENTE/10. AUTENTIFICAREA  &  AUTORIZAREA/SETAREA  HEADER-ELOR  DE  RASPUNS/routes/autentificare.js
+++ COMPONENTE/10. AUTENTIFICAREA  &  AUTORIZAREA/SETAREA  HEADER-ELOR  DE  RASPUNS/routes/autentificare.js
@@ -1,15 +1,20 @@
 // INCARCARI (IMPORTURI):
 // ____________________________________________________________________________
-// (IMP-7) IMPORTAREA BIBLIOTECI 'JSOM WEB TOKEN':
+// (IMP-9) IMPORTAREA  MODULULUI 'CONFIG':
+const config = require('config');
+
+// ____________________________________________________________________________
+// (IMP-8) IMPORTAREA BIBLIOTECI 'JSON WEB TOKEN':
 const jwt = require('jsonwebtoken');
 
 // ____________________________________________________________________________
-// (IMP-8) IMPORTAREA BIBLIOTECI 'CONFIG':
-const config = require('config');
+// (IMP-7) IMPORTAREA BIBLIOTECI 'JOI':
+const Joi = require('joi');
 
 // ____________________________________________________________________________
 // (IMP-6) IMPORTAREA BIBLIOTECI 'BCRYPT':
 const bcrypt = require('bcrypt');
+
 
 // ____________________________________________________________________________
 // (IMP-5) IMPORTAREA BIBLIOTECI 'LODASH' - DENUMITA CONVENTIONAL '_':  
@@ -18,7 +23,7 @@ const _ = require('lodash');
 // ____________________________________________________________________________
 // (IMP-4) IMPORTAREA 'MODEL/UTILIZATOR.JS'  
 //         (RETURNEAZA '.Utilizator' SI '.validare')
-const { Utilizator, validare } = require('../models/utilizator');
+const { Utilizator } = require('../models/utilizator');
 
 // ____________________________________________________________________________
 // (IMP-3) IMPORTAREA 'MONGOOSE'
@@ -44,60 +49,33 @@ const router = express.Router();
 // ____________________________________________________________________________
 router.post('/', async(req, res) => {
 
-    // VALIDAREA 1:
+    // VALIDAREA 1 -----------------------------------------------------------
     // DESTRUCTURAREA OBIECTELOR - APELAREA FUNC. 'VALIDAREGEN()'
     const { error } = validare(req.body);
 
     // LOGICA:  DACA 'GENUL ESTE INVALID' -> RETURNAM '400' (CERERE ERONATA)
     // VERIFICAREA VALOAREI 'REZULTAT' -> A PROP. 'ERROR'
-    if (error)
-    //  RETURNAM 'EROAREA CU STATREA - 400' CATRE 'CLIENT':
-        return res.status(400).send(error.details[0].message);
+    if (error) return res.status(400).send(error.details[0].message);
 
 
-    // VALIDAREA 2:
+    // VALIDAREA 2 -----------------------------------------------------------
     // NE ASIGURAM CA 'UTILIZATORUL' NU ESTE 'DEJA INREGISTRAT'
     // CARE VA RETURNEAZA O 'PROMISIUNE' - PE CARE O 'AWAYT': 
     let utilizator = await Utilizator.findOne({ email: req.body.email });
 
-    // DACA 'UTILIZATORUL EXISTA' IN 'BAZA DE DATE'
+    // DACA 'UTILIZATORUL NU EXISTA' IN 'BAZA DE DATE'
     //  RETURNAM 'EROAREA CU STATREA - 400' CATRE 'CLIENT':
-    if (utilizator) return res.status(400).send('Utilizator deja înregistrat.');
+    if (!utilizator) return res.status(400).send('Email sau parola invalida.');
 
 
-    // RESETAREA - OBIECTULUI 'UTILIZATOR':
-    // utilizator = new Utilizator({
-    //     nume: req.body.nume,
-    //     email: req.body.email,
-    //     parola: req.body.parola
-    // });
 
-    // RESETAREA - OBIECTULUI 'UTILIZATOR'
-    // CU MET. 'PICK(OBIECT, ['PROP1','PROP2'.. ])':
-    utilizator = new Utilizator(_.pick(req.body, ['nume', 'email', 'parola']));
+    // VALIDAREA 3 -----------------------------------------------------------
+    // VALIDAREA 'PAROLEI'
+    // MET. 'COMPARE(PAROLA_TEXT_PLAN, PAROLA_HASH-URATA)' :
+    const parolaValida = await bcrypt.compare(req.body.parola, utilizator.parola);
 
-
-    // MET. ASINCRON 'GENSALT(NR_CARACTERE, CALLBACK_FUNC)' SAU 'PROMISIUNE'
-    // (STRING RENDOM - ADAUGAT INAINTE / DUPA PAROLA):
-    const salt = await bcrypt.genSalt(10);
-
-    // MET. 'HASH('PAROLA', SALT, CALLBACK)' SAu 'PROMISIUNE'  PT HASURAREA PAROLEI:
-    utilizator.parola = await bcrypt.hash(utilizator.parola, salt);
-
-
-    // SALVAREA 'UTILIZATORULUI' IN 'BAZA DE DATE'
-    await utilizator.save();
-
-
-    // RETURNAM 'RASPUNSULUI' - 'UTILIZATOR' CATRE 'CLIENT'
-    // res.send(utilizator);
-
-
-    // (ABORDAREA 1) RETURNAM 'RASPUNSULUI' - 'UTILIZATOR' UN 'OBIECT PERSONALIZAT':
-    // res.send({
-    //     nume: utilizator.nume,
-    //     email: utilizator.email
-    // });
+    // DACA 'PAROLA NU ESTE VALIDA' - RETURNAM 'EROAREA 400':
+    if (!parolaValida) return res.status(400).send('Email sau parola invalida.');
 
 
     // JSON WEB TOKEN (JWT)
@@ -105,13 +83,35 @@ router.post('/', async(req, res) => {
     // APELAREA MET. 'CONGIG.GET()' CA ARGUMENT:
     const token = jwt.sign({ _id: utilizator._id }, config.get('cheiaPrivataJWT'));
 
-
-    // (ABORDAREA 2) BIBLIOTECA 'LODASH' - '_'
-    // CU MET. 'PICK(OBIECT, ['PROP1','PROP2'.. ])'
-    // SI MET. 'HEADER('PREFIX-NUME_ALEATORIU', VALOARE)':
-    res.header('x-autentificare-token', token).send(_.pick(utilizator, ['_id', 'nume', 'email']));
+    // TRIMTEREA RASPUNSULUI 'TOKEN' CATRE 'CLIENT':
+    res.send(token);
 });
 // ____________________________________________________________________________
+
+
+
+
+
+
+
+
+// ____________________________________________________________________________
+// FUNC. 'VALIDARE(REQUEST)'
+// ____________________________________________________________________________
+function validare(req) {
+    // VALIDARE UTILIZATOR
+    // DEF. 'SCHEMEI JOI' = 'OBIECT':
+    const schema = {
+        email: Joi.string().min(5).max(255).required().email(),
+        parola: Joi.string().min(5).max(255).required()
+    };
+
+
+    // RETURNAREA - APELARI MET. JOI 'VALIDATE()':
+    return Joi.validate(req, schema);
+};
+// ____________________________________________________________________________
+
 
 
 
