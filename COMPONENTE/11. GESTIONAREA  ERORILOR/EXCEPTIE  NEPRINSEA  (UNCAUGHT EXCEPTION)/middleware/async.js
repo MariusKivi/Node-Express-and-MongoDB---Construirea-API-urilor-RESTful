aecc67df21d312b17dd556d1d7e0db28c721819d
Chivu Marius <chivumarius@yahoo.com>
2021-09-22 10:40:09 +0300

    Exceptie Neprinsa ('uncaughtException')

---
+++ COMPONENTE/11. GESTIONAREA  ERORILOR/EXCEPTIE  NEPRINSEA  (UNCAUGHT EXCEPTION)/middleware/async.js
@@ -0,0 +1,20 @@
+// EXPORTAREA - FUNC. 'ASYNC MIDDLEWARE()'
+// (CE VA FI PRECUM O 'FUNC. DE FABRICARE'
+//  PE CARE O APELAM SI PRIMIM O 'NOUA FUNC.')
+dodule.exports = function asyncMiddleware(handler) {
+
+    // RETURNAREA 'FUNC. ASINCRON' DE 'GESTIONARE A RUTEI':
+    return async(req, res, next) => {
+        // BLOCURILE DE INCERCARE:
+        try {
+
+            // APELARE FUNC. ASINCRON 'HANDLER()':
+            await handler(req, res);
+
+        } catch (exception) {
+
+            // APELARE:
+            next(exception);
+        }
+    };
+}
---
+++ COMPONENTE/11. GESTIONAREA  ERORILOR/EXCEPTIE  NEPRINSEA  (UNCAUGHT EXCEPTION)/middleware/error.js
@@ -0,0 +1,26 @@
+// ____________________________________________________________________________
+// (IMP-1 IMPORTAREA  BIBLIOTECiI 'WINSTON'
+//  ('IMPLICIT' ESTE PT. 'LOGAREA' - 'TRANSPORTULUI' IN 'CONSOLA'):
+const winston = require('winston');
+
+
+
+
+// ____________________________________________________________________________
+// EXPORTAREA FUNC. DE 'GESTIONARE' A 'ERORILOR MIDDLEWARE'  DIN  'EXPRESS'
+module.exports = function(err, req, res, next) {
+    // (1) APELAM 'WINSTON.LOG(NIVEL_DE_IMPORTANTA_AL_ERORI)'
+    // PT. 'AFISAREA ERORI':
+    // winston.log('error', err.message);
+
+    // (2) SAU FOLOSIM MET. 'HELPER':
+    // winston.error(err.message);
+
+    // (3) PUTEM STOCA SI METADATE 'ERR'
+    // MET. 'ERROR(MESAJ, METADATA)':
+    winston.error(err.message, err);
+
+    // RETURNAM 'RASPUNSULUI' CATRE 'GENuri'
+    // EROAREA '500' - 'EROARE INTERNA DE  SERVER'
+    res.status(500).send('Ceva a eșuat.');
+}
