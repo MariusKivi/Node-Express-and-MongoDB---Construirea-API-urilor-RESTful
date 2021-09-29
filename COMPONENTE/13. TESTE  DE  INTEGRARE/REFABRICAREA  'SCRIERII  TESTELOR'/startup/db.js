17df2c5e82e0509d194d4d10658084f0ee821431
Chivu Marius <chivumarius@yahoo.com>
2021-09-29 21:28:16 +0300

    Refabricarea 'Scrieri Testelor'

---
+++ COMPONENTE/13. TESTE  DE  INTEGRARE/REFABRICAREA  'SCRIERII  TESTELOR'/startup/db.js
@@ -0,0 +1,21 @@
+// (IMP-12 IMPORTAREA  BIBLIOTECiI 'WINSTON'
+//  ('IMPLICIT' ESTE PT. 'LOGAREA' - 'TRANSPORTULUI' IN 'CONSOLA'):
+const winston = require('winston');
+const mongoose = require('mongoose');
+const config = require('config');
+
+
+
+// EXPORTAM FUNCTIA ANONIMA:
+module.exports = function() {
+
+    // CONSTANTA 'DB':
+    const db = config.get('db');
+
+    // CONECTAREA DINAMICA LA  'BAZA DE DATE' - 'MONGODB'
+    // BAZATA PE 'ENVIRONMENT'-UL IN CARE APLICATIA 'RULEAZA':
+    mongoose.connect(db)
+        // 'PROMISIUNE' - IN 'CAZ DE SUCCES':
+        // .then(() => console.log('Conectat cu succes la Baza de Date MongoDB'))
+        .then(() => winston.info(`Conectat cu succes la ${db}...`));
+}
